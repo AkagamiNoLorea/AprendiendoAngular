0622d9bd5f8498c4199cea51bd91545662cc14c6
AkagamiNoLorea <raiseta92@gmail.com>
2024-07-20 11:37:04 +0200

    adding components and string interpolation

---
+++ src/app/clases.component.ts
@@ -0,0 +1,11 @@
+
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'clases',
+    template: `<h1>Mis Clases</h1>
+    <p>Damn backticks</p>`,	
+    styles: ['h1 { color: red; }']
+})
+
+export class ClasesComponent {}
---
+++ src/app/cursos/cursos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cursos',
+  template: '<h1>Mis Cursos<h1>',
+  styleUrls: ['./cursos.component.scss']
+})
+export class CursosComponent {
+
+}
