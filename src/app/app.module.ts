fc216df43a284dd3ada339e24fac5c9c9e1dc857
AkagamiNoLorea <raiseta92@gmail.com>
2024-07-20 16:03:50 +0200

    property binding and class binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,17 @@ import { AppComponent } from './app.component';
 import { ClasesComponent } from './clases.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { PropertyBindingComponent } from './property-binding.component';
+import { ClassBindingComponent } from './class-binding.component';
+import { StyleBindingComponent } from './style-binding.components';
 
 @NgModule({
   declarations: [
-    AppComponent, ClasesComponent, CursosComponent, PropertyBindingComponent
+    AppComponent,
+    ClasesComponent,
+    CursosComponent,
+    PropertyBindingComponent,
+    ClassBindingComponent,
+    StyleBindingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/class-binding.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'class-binding',
+    template: `<button [class.activo] = "isActive">Mi boton</button>`,
+    /*el Class binding se encarga de añadir una determimada clase a un elemento en base a una condición */
+    styles: [`
+        .activo{
+            background-color: red;
+            color: white;
+        }`]
+})
+
+export class ClassBindingComponent {
+    isActive = true;
+
+}
